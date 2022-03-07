d4b4b46a51aaaa53cb55e040b71da30a9b11e599
Kevin Dudley <kjdudley@Kevins-MacBook-Pro.local>
2022-03-07 11:48:49 +1000

    Change to an org-mode generated website.

---
+++ build-site.el
@@ -0,0 +1,47 @@
+(require 'package)
+(setq package-user-dir (expand-file-name "./.packages"))
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			  ("org" . "https://orgmode.org/elpa/")
+			  ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(package-install 'ox-tufte)
+
+;; load the publishing system
+(require 'ox-publish)
+
+(require 'ox-tufte)
+
+;; Customize the html output
+(setq org-html-validation-link nil
+      org-html-head-include-scripts nil
+      org-html-head-include-default-style nil
+      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple-v1.css\" />")
+      ;;org-html-head "<link rel=\"stylesheet\" type \"text/css\" href=\"/Users/kjdudley/Documents/my-org-site/tufte.css\" />")
+
+;; Define the publishing project
+(setq org-publish-project-alist
+      (list
+       (list "my-org-site"
+	     :html-container "section"
+	     :html-doctype "html5"
+	     :headline-levels 2
+	     :html-html5-fancy t
+	     :recusive t
+	     :base-directory "./content"
+	     :publishing-directory "./public"
+	     :publishing-function 'org-html-publish-to-tufte-html
+	     :with-author nil
+	     :with-creator t
+	     :with-toc nil
+	     :with-footnotes t
+	     :section-numbers nil
+	     :time-stamp-file nil)))
+
+;; Generate the site output
+(org-publish-all t)
+
+(message "Build complete!")
